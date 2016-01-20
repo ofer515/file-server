e3f9af6e09a51b45a96a47c2bfc5e7563893cd90
misha <bla@gmail.com>
2016-01-20 16:43:38 +0200

    second commit
    just a commit

--- client.py
+++ client.py
@@ -4,11 +4,11 @@ import socket,os
 
 client_soc = socket.socket()
 client_soc.connect(("127.0.0.1", 5679))
+while True:
+    client_soc.send("sending file name:" + raw_input("file name to send: "))
+    
 
-client_soc.send("did you get it?")
-
-
-#client_soc.close()
+client_soc.close()
 
 
 
--- server.py
+++ server.py
@@ -7,32 +7,34 @@ import thread
 ser_socket = socket.socket()
 
 
-
-
 def main(port):
     ser_socket.bind(("127.0.0.1", port))
-    ser_socket.settimeout(20)
+    ser_socket.settimeout(10)
     ser_socket.listen(10)
 
     while True:
-
-        client_socket, client_address = ser_socket.accept()
         try:
-            ack_msg = ser_socket.recv(1024)
+            client_socket, client_address = ser_socket.accept()
+            print "new connection"
+        except:
+            pass
+
+        try:
+            print "h"
+            ack_msg = client_socket.recv(1024)
             if ack_msg.contain("sending file name:"):
                 fName = ack_msg.split(":", 1)
+                print "ads"
                 recv_thread = thread.start_new_thread(recv_files, fName)
             else:
                 ser_socket.send("not in protocol")
-        except socket.timeout:
+                print "not in protocol"
+                client_socket.close()
+        except Exception as e:
+            print e
             ser_socket.send("connection timed out")
-            print "connection timed out"
+
             client_socket.close()
-
-   #send_thread = thread.start_new_thread()
-
-def send_file():
-    pass
 
 def recv_files(fname):
 
