ce0226821dd8b56ce3219765e64cda39efb62785
ofer515 <oferikko97@gmail.com>
2016-01-23 22:50:04 +0200

    Day 3 final changes
    uploading works, downloading should be the same way' can start working
    on checking the file validation

--- client/client.py
+++ client/client.py
@@ -1,28 +1,64 @@
-import socket,os
+import socket
+import os
+
+allowed_files = ['txt', 'png', 'jpg','doc', 'docx']
 
 
+def files_available(files):
+    file_list = []
+    for file_name in files:
+        file_type = file_name.split('.')[-1]
+        if file_type in allowed_files:
+            file_list.append(file_name)
+    if file_list == []:
+        return "no file available"
+    return file_list
 
-client_soc = socket.socket()
-client_soc.connect(("127.0.0.1", 5679))
 
-while True:
+def send_file(up_file_name, client_soc):
+    up_file = open(up_file_name, 'rb')
+    temp_holder = up_file.read(1024)
+    while temp_holder:
+        client_soc.send(temp_holder)
+        temp_holder = up_file.read(1024)
+    print "send successfully"
+    client_soc.close()
 
+def main(file_list):
+    client_soc = socket.socket()
+    client_soc.connect(("127.0.0.1", 5679))
 
     data = client_soc.recv(1024)
     if data == "Approved":
         print "Approved"
-        option = int(raw_input("upload(1) or download(2)?: "))
-        if option == 1:
-            client_soc.send("upload")
-            raw_data = "sending file name:" + raw_input("file name to send: ")
-            print(raw_data)
-            client_soc.send(raw_data)
+        while True:
+
+            option = int(raw_input("upload(1), download(2) or exit(3) ?: "))
+            if option == 1:
+                client_soc.send("upload")
+                print "files available to upload"
+                print(file_list)
+                up_file_name = str(raw_input("file name to send: "))
+                if up_file_name in file_list:
+                    raw_data = "sending file name:"
+                    print(raw_data+" "+up_file_name)
+                    client_soc.send(raw_data+up_file_name)
+                    print "sending......"
+                    send_file(up_file_name, client_soc)
+                else:
+                    print("no such file is available, try again")
+            elif option == "3":
+                print("closing connection")
+                client_soc.close()
     else:
-        print "no answer"
+        print "server is down or too busy "
         print data
 
-client_soc.close()
+    client_soc.close()
 
+
+if __name__ == "__main__":
+    main(files_available(os.listdir(os.curdir)))
 
 
 
--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-import socket,os
+import socket
+import os
 import thread
 
 
@@ -10,53 +11,69 @@ ser_socket = socket.socket()
 def main(port):
     ser_socket.bind(("127.0.0.1", port))
     ser_socket.settimeout(100)
-    ser_socket.listen(100)
+    ser_socket.listen(10)
 
     while True:
         try:
             client_socket, client_address = ser_socket.accept()
-            client_socket.setblocking(20)
-            soc_status = True
-            print "new connection"
-            client_socket.send("Approved")
-            print "Approved"
-            request = client_socket.recv(1024)
-            print "got"
-            if request == "upload":
-                recv_thread = thread.start_new_thread(recv_files, ((client_socket,)))
-            elif request is "download":
-                pass
-            else:
-                ser_socket.close()
-        except Exception as e:
-            print e
+            handle_thread = thread.start_new_thread(handle_connection, ((client_socket, client_address)),)
+            if handle_thread:
+                print "saved file"
+
+        except:
+            pass
 
 
 def recv_files(client_soc):
     try:
         ack_msg = client_soc.recv(1024)
-
+        print(ack_msg.split(":"))
         if "sending file name:" in ack_msg:
-            f_name = str(ack_msg.split(":", 1)[1])
+            f_name = str(ack_msg.split(":", 1)[-1])
+            print(f_name)
+            new_file = open(f_name, 'wb')
             msg = str("receiving file name: "+f_name)
             print msg
-            print f_name
+            up_file = client_soc.recv(1024)
+            while up_file:
+                print "receiving...."
+                new_file.write(up_file)
+                up_file = client_soc.recv(1024)
+            new_file.close()
+            print "received file and saved"
+            client_soc.close()
 
         else:
             client_soc.send("not in protocol")
             print "not in protocol, try reconnecting"
             client_soc.close()
     except Exception as e:
-        print e
+        if e == "[Errno 10054] An existing connection was forcibly closed by the remote host":
+            print("user disconnected")
+        else:
+            print e
 
-    print "waiting for file...."
-    file = client_soc.recv(1024)
-    nFile = open(fname, 'w+')
-    while(file):
-        print "receiving...."
-        nFile.write(file)
-    nFile.close()
 
+
+def handle_connection(client_soc, client_addr):
+    try:
+        client_soc.setblocking(100)
+        soc_status = True
+        print "new connection"
+        client_soc.send("Approved")
+        print "Approved"
+        request = client_soc.recv(1024)
+        if request == "upload":
+            recv_files(client_soc)
+        elif request is "download":
+            pass
+        else:
+            ser_socket.close()
+    except Exception as e:
+        if e == "[Errno 10054] An existing connection was forcibly closed by the remote host":
+            print("user disconnected")
+        else:
+            print e
 
 
 
